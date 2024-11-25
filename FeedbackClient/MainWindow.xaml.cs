e929dd9eda8edc2a8ceb82342c74312e67a27ccf
zhenghan <zhbln8499apple@live.com>
2024-11-25 14:27:41 +0800

    Add project files.

---
+++ FeedbackClient/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FeedbackClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<OrderPOCO> _orderPOCOs;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbCoCode.Text = ConfigurationManager.AppSettings["CompanyCode"];
+            tbSortingLineCode.Text = ConfigurationManager.AppSettings["SortLineCode"];
+            _orderPOCOs = GetOrderDatas();
+        }
+        private List<OrderPOCO> GetOrderDatas()
+        {
+            var data = DAO.getPack(1);
+            var list = new List<OrderPOCO>();
+            int index = 0;
+            foreach (var item in data)
+            {
+                list.Add(new OrderPOCO
+                {
+                    Amount = int.Parse(item["need_num"].ToString()),
+                    CustomerCode = item["customer_code"].ToString(),
+                    CustomerName = item["customer_name"].ToString(),
+                    DeliveryName = item["route_name"].ToString(),
+                    OrderID = item["order_id"].ToString(),
+                    ProductName = item["product_name"].ToString(),
+                    TagAddress = item["tag_id"].ToString(),
+                    State = item["state"].ToString() == "0" ? "未分拣" : "已分拣",
+                    DeliverySeq = int.Parse(item["deliver_seq"].ToString()),
+                    TotalAmount = int.Parse(item["sum_num"].ToString()),
+                    DeliveryCode = item["route_id"].ToString(),
+                    PackingMaximumNO = int.Parse(item["max_pack"].ToString()),
+                    SortingNO = int.Parse(item["sort_num"].ToString()),
+                    PackingNO = int.Parse(item["pack_num"].ToString()),
+                    OrderNO = int.Parse(item["order_sort"].ToString()),
+                    Index = index,
+                    ProductID = int.Parse(item["product_id"].ToString())
+                });
+                index++;
+            }
+            var p = list.GroupBy(g => g.OrderID);
+            foreach (var item in p)
+            {
+                var q = item.GroupBy(g => g.TagAddress).Where(w => w.Count() > 1);
+                if (q != null && q.Count() > 0)
+                {
+                    foreach (var it in q)
+                    {
+                        int amount = 0;
+                        var tmp = new List<int>();
+                        foreach (var im in it)
+                        {
+                            tmp.Add(im.Index);
+                            amount += im.Amount;
+                        }
+                        for (int i = 0; i < tmp.Count; i++)
+                        {
+                            if (i == 0)
+                            {
+                                list.FirstOrDefault(f => f.Index == tmp[i]).Amount = amount;
+                            }
+                            else
+                            {
+                                list.Remove(list.FirstOrDefault(f => f.Index == tmp[i]));
+                            }
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+        private async void SendFeedback_Click(object sender, RoutedEventArgs e)
+        {
+            var p = _orderPOCOs.GroupBy(g => g.OrderID).ToList();
+            var q = p.Sum(s => s.First().TotalAmount);
+            var co_list = new List<COList>();
+            foreach (var co in p)
+            {
+                var CO_DETAIL_LIST = new List<CODetail>();
+                foreach (var co2 in co)
+                {
+                    CO_DETAIL_LIST.Add(new CODetail() { ITEM_ID = co2.ProductID.ToString(), QTY = co2.Amount });
+                }
+                co_list.Add(new COList { PICK_CO_NUM = co.First().SortingNO.ToString(), SEQ = co.First().SortingNO, CO_NUM = co.First().CustomerCode, CO_DETAIL_LIST = CO_DETAIL_LIST });
+            }
+            var dataset = new DataSet
+            {
+                PICK_TASK_NUM = tbTaskNO.Text.Trim(),
+                COM_ID = tbCoCode.Text.Trim(),
+                PICK_LINE_ID = tbSortingLineCode.Text.Trim(),
+                PICK_DATE = DateTime.Now.ToString("yyyyMMdd"),
+                ISSUE_TIME = DateTime.Now.ToString("yyyyMMdd"),
+                CUST_COUNT = _orderPOCOs.GroupBy(g => g.CustomerCode).Count(),
+                QTY_SUM = _orderPOCOs.GroupBy(g => g.OrderID).ToList().Sum(s => s.First().TotalAmount), CO_LIST = co_list
+            };
+            string apiUrl = tbUrl.Text.Trim();
+            var request = new FeedbackRequest() { DATASET = new List<DataSet>()};
+            request.DATASET.Add(dataset);
+
+            string sendData = JsonConvert.SerializeObject(request);
+            tbJsonText.Text = sendData;
+            try
+            {
+                var response = await SendDataToServer(apiUrl, sendData);
+                tbResponse.Text = $"消息: {response.ToString()}";
+                tbResponse.Foreground = new SolidColorBrush(Colors.Green);
+            }
+            catch (Exception ex)
+            {
+                tbResponse.Text = $"发生异常:{ex.Message}";
+                tbResponse.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+        private async void TestConnect_Click(object sender, RoutedEventArgs e)
+        {
+            string apiUrl = tbUrl.Text.Trim();
+            string sendData = tbJsonText.Text;
+            try
+            {
+                var parseJson = JsonConvert.DeserializeObject(sendData);
+                if (parseJson == null)
+                {
+                    tbResponse.Text = "数据格式不正确";
+                    tbResponse.Foreground = new SolidColorBrush(Colors.Red);
+                    return;
+                }
+                var response = await SendDataToServer(apiUrl, sendData);
+                tbResponse.Text = $"消息: {response.ToString()}";
+                tbResponse.Foreground = new SolidColorBrush(Colors.Green);
+            }
+            catch (Exception ex)
+            {
+                tbResponse.Text = $"发生异常:{ex.Message}";
+                tbResponse.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+        private async Task<string> SendDataToServer(string url, string data)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                var content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
---
+++ FeedbackClient/Model.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackClient
+{
+    public class FeedbackRequest
+    {
+        public List<DataSet> DATASET { get; set; }
+    }
+
+    public class DataSet
+    {
+        /// <summary>
+        /// 分拣任务号
+        /// </summary>
+        public string PICK_TASK_NUM { get; set; }
+        /// <summary>
+        /// 分拣日期
+        /// </summary>
+        public string PICK_DATE { get; set; }
+        /// <summary>
+        /// 公司编码
+        /// </summary>
+        public string COM_ID { get; set; }
+        /// <summary>
+        /// 分拣线编码
+        /// </summary>
+        public string PICK_LINE_ID { get; set; }
+        /// <summary>
+        /// 零售户总数
+        /// </summary>
+        public int CUST_COUNT { get; set; }
+        /// <summary>
+        /// 分拣总量(条)
+        /// </summary>
+        public int QTY_SUM { get; set; }
+        /// <summary>
+        /// 下发条零关联时间
+        /// </summary>
+        public string ISSUE_TIME { get; set; }
+        public List<COList> CO_LIST { get; set; }
+    }
+
+    public class COList
+    {
+        /// <summary>
+        /// 分拣执行订单编号
+        /// </summary>
+        public string PICK_CO_NUM { get; set; }
+        /// <summary>
+        /// 分拣顺序号
+        /// </summary>
+        public int SEQ { get; set; }
+        /// <summary>
+        /// 零售户订单编码
+        /// </summary>
+        public string CO_NUM { get; set; }
+        public List<CODetail> CO_DETAIL_LIST { get; set; }
+    }
+
+    public class CODetail
+    {
+        /// <summary>
+        /// 短号
+        /// </summary>
+        public string ITEM_ID { get; set; }
+        /// <summary>
+        /// 分拣数量(条)
+        /// </summary>
+        public int QTY { get; set; }
+    }
+    /// <summary>
+    /// 订单基础对象
+    /// </summary>
+    class OrderBaseModel
+    {
+        /// <summary>
+        /// 索引
+        /// </summary>
+        public int Index { get; set; }
+        /// <summary>
+        /// 分拣序号
+        /// </summary>
+        public int SortingNO { get; set; }
+        /// <summary>
+        /// 包序号
+        /// </summary>
+        public int PackingNO { get; set; }
+        /// <summary>
+        /// 最大包数量
+        /// </summary>
+        public int PackingMaximumNO { get; set; }
+        /// <summary>
+        /// 订单标识
+        /// </summary>
+        public string OrderID { get; set; }
+        /// <summary>
+        /// 烟名
+        /// </summary>
+        public string ProductName { get; set; }
+        /// <summary>
+        /// 电子标签地址
+        /// </summary>
+        public string TagAddress { get; set; }
+        /// <summary>
+        /// 当前条烟所需的数量
+        /// </summary>
+        public int Amount { get; set; }
+        public int ProductID { get; set; }
+    }
+    class OrderPOCO : OrderBaseModel
+    {
+        /// <summary>
+        /// 分拣状态
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// 客户名
+        /// </summary>
+        public string CustomerName { get; set; }
+        /// <summary>
+        /// 客户编码
+        /// </summary>
+        public string CustomerCode { get; set; }
+        /// <summary>
+        /// 送货序号
+        /// </summary>
+        public int DeliverySeq { get; set; }
+        /// <summary>
+        /// 送货线路名
+        /// </summary>
+        public string DeliveryName { get; set; }
+        /// <summary>
+        /// 送货线路编码
+        /// </summary>
+        public string DeliveryCode { get; set; }
+        /// <summary>
+        /// 订单下的总条烟数量
+        /// </summary>
+        public int TotalAmount { get; set; }
+        /// <summary>
+        /// 订单序号
+        /// </summary>
+        public int OrderNO { get; set; }
+    }
+
+}
